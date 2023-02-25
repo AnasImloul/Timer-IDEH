72e4b453daa35757b8f3649aeafc8597c664ab4a
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-02-25 21:30:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,183 +1,35 @@
-var counter = 0;
-var interval = 0;
-var startDate = undefined;
-var endDate = undefined;
-var date;
-var clicked = false;
-var resetColor = true;
-var before = false;
-var initDate = undefined;
-var globalDate = undefined;
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Consolas" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="style.css">
+</head>
 
 
-function addSeconds(date, seconds) {
-  date.setSeconds(date.getSeconds() + seconds);
-  return date;
-}
+<body>
+  <img class="ideh" id="ideh" src="https://user-images.githubusercontent.com/76872415/221301905-6e89c7dc-ba7b-4749-852c-f5249744c5a7.png"/>
 
-function parseString(value) {
-  if (value >= 10) {
-    return "" + value;
-  }
-  return "0" + value;
-}
+  <div class="logo-container">
+    <img class="cit" id="cit" src="https://user-images.githubusercontent.com/76872415/221301909-1682023b-33a7-4eb4-8ade-e56658a3dc8a.png"/>
+    <img class="thnb" id="thnb" src="https://user-images.githubusercontent.com/76872415/221375629-7fd61049-ddda-4a65-b50e-9f5ef645e0db.png"/>
+  </div>
 
-function toString(hours, minutes, seconds) {
-  return (
-    parseString(hours) + ":" + parseString(minutes) + ":" + parseString(seconds)
-  );
-}
+  <div id="timer">
+    <div id="counter">00:00:00</div>
+    <div id="description">Until the CTF begins</div>
+  </div>
 
-function updateTimer() {
-  if (!clicked) {
-    return;
-  }
+  <button id="start" type="button" onclick="startTimer()">start</button>
 
-  date = addSeconds(new Date(), Math.floor((globalDate - initDate) / 1000));
+  <div class="picker" id="picker">
+    <div class="start_picker">
+      <input id="date-picker-start" type="date" value="2023-02-26"/>
+      <input id="time-picker-start" type="time" value="00:00"/>
+    </div>
 
+    <div class="stop_picker-end">
+      <input id="date-picker-end" type="date" value="2023-02-26"/>
+      <input id="time-picker-end" type="time" value="09:00"/>
+    </div>
+  </div>
+</body>
 
-  if (date < startDate) {
-    beforeContest();
-  } else if (date <= endDate) {
-    inContest();
-  } else {
-    endContest();
-  }
-
-  updateCounter();
-}
-
-function blinkTimer() {
-  if (counter >= 60 || !clicked) {
-    return;
-  }
-  var color = document.getElementById("counter").style.color;
-  if (color == "white") {
-    color = "red";
-  } else {
-    color = "white";
-  }
-  document.getElementById("counter").style.color = color;
-  if (before) {
-    document.getElementById("description").innerHTML = "Get Ready, Good Luck!";
-  }
-}
-
-function updateCounter() {
-  hours = Math.floor(counter / 3600);
-  minutes = Math.floor((counter % 3600) / 60);
-  seconds = counter % 60;
-
-  if (counter < 60) {
-    document.getElementById("counter").innerHTML = "" + seconds;
-  } else {
-    document.getElementById("counter").innerHTML = toString(
-      hours,
-      minutes,
-      seconds
-    );
-  }
-}
-
-function endContest() {
-  counter = 0;
-  hide_counter();
-  document.getElementById("description").innerHTML =
-    "the CTF contest has Ended!";
-  before = false;
-}
-
-function inContest() {
-  document.getElementById("description").innerHTML =
-    "Until the CTF contest ends";
-  counter = endDate - date;
-  counter = Math.floor(counter / 1000);
-
-  if (resetColor) {
-    document.getElementById("counter").style.color = "white";
-    resetColor = false;
-  }
-  before = false;
-}
-
-function beforeContest() {
-  counter = startDate - date;
-  counter = Math.floor(counter / 1000);
-  before = true;
-}
-
-function hide_counter() {
-  document.getElementById("counter").style.display = "None";
-}
-
-function hide_picker() {
-  document.getElementById("picker").style.display = "None";
-}
-
-function hide_start() {
-  document.getElementById("start").style.display = "None";
-}
-
-function show_ideh() {
-  document.getElementById("ideh").style.display = "block";
-  document.getElementById("description").style.display = "block";
-}
-
-function hide_ideh() {
-  document.getElementById("ideh").style.display = "None";
-  document.getElementById("description").style.display = "None";
-}
-
-async function current_date() {
-  try {
-    const response = await fetch('https://worldtimeapi.org/api/timezone/Etc/UTC');
-    const data = await response.json();
-    const date = new Date(data.utc_datetime);
-    return date;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function startTimer() {
-
-  var startDateInput = document.getElementById("date-picker-start").value;
-  var startTimeInput = document.getElementById("time-picker-start").value;
-
-  var endDateInput = document.getElementById("date-picker-end").value;
-  var endTimeInput = document.getElementById("time-picker-end").value;
-
-  if (
-    startDateInput == "" ||
-    startTimeInput == "" ||
-    endDateInput == "" ||
-    endTimeInput == ""
-  ) {
-    return;
-  }
-
-  hide_picker();
-  hide_start();
-  show_ideh();
-
-  var startDatetime = startDateInput + " " + startTimeInput;
-  startDate = new Date(startDatetime);
-
-  var endDateTime = endDateInput + " " + endTimeInput;
-  endDate = new Date(endDateTime);
-
-  if (startDate > endDate){
-    return;
-  }
-
-  interval = Math.floor((endDate - startDate) / 1000);
-
-  globalDate = await current_date();
-  initDate = new Date();
-
-  clicked = true;
-}
-
-
-setInterval(updateTimer, 10);
-setInterval(blinkTimer, 500);
+<script src="script.js"></script>
